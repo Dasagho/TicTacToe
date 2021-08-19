b03b294d9c53ece607aac66d7542e1393dac0df3
pepe05422 <davidamir03@gmail.com>
2021-08-19 20:25:39 +0200

    first design and failed attemp to JS

---
+++ main.js
@@ -0,0 +1,16 @@
+    let cell1 = document.getElementById("c1");
+    let cell2 = document.getElementById("c2");
+    let cell3 = document.getElementById("c3");
+    let cell4 = document.getElementById("c4");
+    let cell5 = document.getElementById("c5");
+    let cell6 = document.getElementById("c6");
+    let cell7 = document.getElementById("c7");
+    let cell8 = document.getElementById("c8");
+    let cell9 = document.getElementById("c9");
+
+    let table = [   [c1, c2, c3],
+                    [c4, c5, c6], 
+                    [c7, c8, c9]
+                ];
+    
+    
